41411a92cb0fb497b7c24fc88d04ad7592112e1a
fabbitox <hyunjin_11@naver.com>
2023-10-04 15:10:18 +0900

    add check

--- backend/flow/src/main/java/fabbitox/flow/repository/WaterLevelJdbc.java
+++ backend/flow/src/main/java/fabbitox/flow/repository/WaterLevelJdbc.java
@@ -1,13 +1,15 @@
 package fabbitox.flow.repository;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import fabbitox.flow.entity.WaterLevel;
 import fabbitox.flow.entity.WaterLevelId;
 import lombok.RequiredArgsConstructor;
 
-//@Repository
-//@Primary
+@Repository
+@Primary
 @RequiredArgsConstructor
 public class WaterLevelJdbc implements WaterLevelRepository {
 	private final JdbcTemplate jdbcTemplate;
@@ -15,7 +17,14 @@ public class WaterLevelJdbc implements WaterLevelRepository {
 	@Override
 	public void insertWaterLevel(WaterLevel waterLevel) {
 		WaterLevelId id = waterLevel.getId();
+		final Integer targetId = id.getTargetId();
+		final String observeDatetime = id.getObserveDatetime();
+		
+		int count = jdbcTemplate.queryForObject(String.format("SELECT COUNT(*) FROM water_level where target_id = %d and observe_datetime = \"%s\"", targetId, observeDatetime), Integer.class);
+		if (count > 0)
+			return;
+		
 		jdbcTemplate.update("INSERT INTO water_level (target_id, observe_datetime, water_level) VALUES (?, ?, ?)",
-				id.getTargetId(), id.getObserveDatetime(), waterLevel.getWaterLevel());
+				targetId, observeDatetime, waterLevel.getWaterLevel());
 	}
 }
